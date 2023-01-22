e98b8d1a3718e8ff9d158107b39177fb204df331
veighnsche <vincepaul.liem@gmail.com>
2023-01-22 02:21:01 +0100

    everything left click in image tags

--- frontend/src/components/outputs/ImageTags.tsx
+++ frontend/src/components/outputs/ImageTags.tsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled'
 import { Chip } from '@mui/material'
-import { MouseEvent } from 'react'
 import { useAppDispatch, useAppSelector } from '../../store'
 import { moveTagBetweenLocations, newTag, selectGetId, selectLocateTagByName } from '../../store/reducers/tags'
 
