8fec156149f2848bceae7eb90fdc42f325b65215
veighnsche <vincepaul.liem@gmail.com>
2023-01-22 02:20:47 +0100

    everything left click in image tags

--- frontend/src/components/outputs/ImageTags.tsx
+++ frontend/src/components/outputs/ImageTags.tsx
@@ -31,28 +31,6 @@ export const ImageTags = ({ tags = [] }: ImageTagsProps) => {
           found,
         } = locateTag(tag)
 
-        /**
-         * on left click:
-         *   if tag is not found:
-         *     add tag to tagPool
-         *   if tag is in tagPool:
-         *     move from tagPool to tags
-         *   if tag is in tags:
-         *     move from tags to tagPool
-         *   if tag is in negativeTags:
-         *     move from negativeTags to tags
-         *
-         * on right click:
-         *   if tag is not found:
-         *     do nothing
-         *   if tag is in tagPool:
-         *     move from tagPool to negativeTags
-         *   if tag is in tags:
-         *     move from tags to negativeTags
-         *   if tag is in negativeTags:
-         *     move from negativeTags to tagPool
-         */
-
         function handleLeftClick() {
           if (!found) {
             dispatch(newTag({ name: tag, location: 'tagPool' }))
@@ -60,22 +38,8 @@ export const ImageTags = ({ tags = [] }: ImageTagsProps) => {
           }
           dispatch(moveTagBetweenLocations({
             id: getId(tag) as string,
-            to: isTagPool || isNegativeTags
-              ? 'tags'
-              : 'tagPool'
+            to: isTagPool ? 'tags' : isTags ? 'negativeTags' : 'tagPool',
           }))
-        }
-
-        function handleRightClick(e: MouseEvent<HTMLDivElement>) {
-          if (found) {
-            e.preventDefault()
-            dispatch(moveTagBetweenLocations({
-              id: getId(tag) as string,
-              to: isTagPool || isTags
-                ? 'negativeTags'
-                : 'tagPool'
-            }))
-          }
         }
 
         return (
@@ -86,7 +50,6 @@ export const ImageTags = ({ tags = [] }: ImageTagsProps) => {
             variant={found ? 'filled' : 'outlined'}
             color={isTags ? 'primary' : isNegativeTags ? 'secondary' : 'default'}
             onClick={handleLeftClick}
-            onContextMenu={handleRightClick}
           />
         )
       })}
